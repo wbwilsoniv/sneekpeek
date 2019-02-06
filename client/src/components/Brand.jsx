02af30fd22f5266c851789ed5eeeff9913740056
wbwilsoniv <wbwilsoniv@gmail.com>
2019-02-06 16:46:39 -0500

    Experienced bug when trying to start client-side server from eslint dependency

--- client/src/components/Brand.jsx
+++ client/src/components/Brand.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function Brand(props) {
-    return (
-        <div className="brand-list" key={brand.id}>
-            <h4>{brand.name}</h4>
-            <img src={brand.pic_url} className="brandLogo" />
-            <span>{brand.founded}</span>
-            <span>{brand.hq}</span>
-            <p>{brand.about}</p>
-        </div>
-    ) 
+  return (
+    <div className="brand-list card" key={brand.id}>
+      <h4 className="card-header-title">{brand.name}</h4>
+      <div className="card-image">
+        <figure class="image is128x128">
+          <img src={brand.pic_url} className="brandLogo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <span className="">{brand.founded}</span>
+        <span className="">{brand.hq}</span>
+        <p className="">{brand.about}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Brand;
